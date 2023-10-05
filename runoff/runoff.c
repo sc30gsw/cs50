3800c744b7816846ca8ec4b0ded778431ae2c4e6
Kaito <74711118+sc30gsw@users.noreply.github.com>
2023-10-05 15:16:01 +0900

    コメントの削除・付与

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -3,20 +3,15 @@
 #include <string.h>
 #include <limits.h>
 
-// 有権者の最大数
 #define MAX_VOTERS 100
-// 候補者の最大数
 #define MAX_CANDIDATES 9
 
-// 投票者番号[i]/投票者iのj番目の選好である候補者のindex
 int preferences[MAX_VOTERS][MAX_CANDIDATES];
 
 typedef struct
 {
     string name;
-    // 現在所有している投票数
     int votes;
-    // 候補者が選挙から除外されたかどうか
     bool eliminated;
 }
 candidate;
@@ -66,13 +61,11 @@ int main(int argc, string argv[])
     // 投票
     for (int i = 0; i < voter_count; i++)
     {
-
         // 優先順位
         for (int j = 0; j < candidate_count; j++)
         {
             string name = get_string("Rank %i: ", j + 1);
 
-            // 無効でない場合、投票を記録
             if (!vote(i, j, name))
             {
                 printf("Invalid vote.\n");
@@ -86,7 +79,6 @@ int main(int argc, string argv[])
     // 勝者が決まるまで決選投票を続ける
     while (true)
     {
-        // 残りの候補者を考慮して票を計算する
         tabulate();
 
         bool won = print_winner();
@@ -111,7 +103,6 @@ int main(int argc, string argv[])
             break;
         }
 
-        // 最低投票数の人を除外
         eliminate(min);
 
         for (int i = 0; i < candidate_count; i++)
@@ -162,6 +153,7 @@ bool print_winner(void)
 
     for (int i = 0; i < candidate_count; i ++)
     {
+        // 過半数投票の投票を得た候補者を出力
         if(candidates[i].votes >= majority_votes)
         {
             printf("%s\n", candidates[i].name);
@@ -186,7 +178,6 @@ int find_min(void)
     return min_votes;
 }
 
-// 候補者間で同点の場合、true
 bool is_tie(int min)
 {
     for (int i = 0; i < candidate_count; i ++)
