f92bae69a98d960feb38c934434838fa92428e3c
Kaito <74711118+sc30gsw@users.noreply.github.com>
2023-10-06 18:21:41 +0900

    size関数の実装

--- week5/speller/dictionary.c
+++ week5/speller/dictionary.c
@@ -75,10 +75,12 @@ bool load(const char *dictionary)
     return true;
 }
 
-// Returns number of words in dictionary if loaded, else 0 if not yet loaded
 unsigned int size(void)
 {
-    // TODO
+    if (wordCount > 0)
+    {
+        return wordCount;
+    }
     return 0;
 }
 
