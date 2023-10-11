d344c17386e1eb2eb278f9f8a8824320516024ae
Kaito <74711118+sc30gsw@users.noreply.github.com>
2023-10-11 14:08:26 +0900

    printの内容をenumで定義したものに修正

--- week6/credit.py
+++ week6/credit.py
@@ -1,16 +1,24 @@
 import re
 
+from enum import Enum
+
+class CardEnum(Enum):
+    AMEX = "AMEX"
+    MASTERCARD = "MASTERCARD"
+    VISA = "VISA"
+    INVALID = "INVALID"
+
 def main():
   try:
     card = int(input("Number: "))
 
     if is_valid_card(card):
-      card_company(card)
+      print(card_company(card).value)
     else:
-      print("INVALID")
+      print(CardEnum.INVALID.value)
     
   except:
-    print("INVALID")
+    print(CardEnum.INVALID.value)
 
 def is_valid_card(card_number):
   # Luhn Algorithmを使用してカード番号をチェック
@@ -34,13 +42,13 @@ def is_valid_card(card_number):
 
 def card_company(card):
   if re.match(r'^3[47][0-9]{13}$', str(card)):
-    print("AMEX")
+    return CardEnum.AMEX
   elif re.match(r'^5[1-5][0-9]{14}$', str(card)):
-    print("MASTERCARD")
+    return CardEnum.MASTERCARD
   elif re.match(r'^4[0-9]{12}(?:[0-9]{3})?$', str(card)):
-      print("VISA")
+    return CardEnum.VISA
   else:
-    print("INVALID")
+    return CardEnum.INVALID
 
 if __name__ == "__main__":
   main()
